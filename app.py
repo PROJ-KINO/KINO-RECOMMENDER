92d5f1835c1b85bc4dc3f4762a1b06640ec4b32c
see101702 <see101702@gmail.com>
2025-07-18 17:12:57 +0900

    포스터 대신 스틸컷

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def recommend():
     movie_info = movie_info.sort_values('order')
 
     # 딕셔너리 리스트로 반환
-    result = movie_info[['movie_id', 'title', 'poster_url']].to_dict(orient='records')
+    result = movie_info[['movie_id', 'title', 'still_cut_url']].to_dict(orient='records')
     return jsonify({'movies': result})
 
 if __name__ == "__main__":
